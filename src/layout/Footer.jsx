b6ce0a8c75c1b4f48038933297f0c20fc08c0190
Khalil <Khalilhashemi.h@gmail.com>
2024-09-01 19:45:35 +0330

    Footer Component finished

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,5 +1,49 @@
+import { Link } from "react-router-dom";
+import {
+  BsTwitter,
+  BsTelegram,
+  BsInstagram,
+  BsFacebook,
+  BsHeartFill,
+} from "react-icons/bs";
+import styles from "./Footer.module.css";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <span>
+          <p>عضویت در خبرنامه</p>
+          <input type="email" placeholder="لطفا ایمیل خود را وارد کنید..." />
+          <button>ارسال</button>
+        </span>
+        <span>
+          <p>شبکه های اجتماعی</p>
+          <Link to="https://twitter.com/">
+            <BsTwitter />
+          </Link>
+          <Link to="https://www.facebook.com/">
+            <BsFacebook />
+          </Link>
+          <Link to="https://www.instagram.com/">
+            <BsInstagram />
+          </Link>
+          <Link to="https://telegram.org/">
+            <BsTelegram />
+          </Link>
+        </span>
+      </div>
+      <div>
+        <p>
+          ساخته شده با
+          <BsHeartFill color="red" />
+          توسط
+          <Link to="https://github.com/KhalilHashemi" target="_blank">
+            Khalil
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,57 +1,11 @@
-import { Link } from "react-router-dom";
-import {
-  BsTwitter,
-  BsTelegram,
-  BsInstagram,
-  BsFacebook,
-  BsHeartFill,
-} from "react-icons/bs";
-import styles from "../styles/Layout.module.css";
 import Header from "./Header";
-
+import Footer from "./Footer";
 function Layout({ children }) {
   return (
     <>
       <Header />
       {children}
-      <footer className={styles.footer}>
-        <div>
-          <span>
-            <p>عضویت در خبرنامه</p>
-            <input type="email" placeholder="لطفا ایمیل خود را وارد کنید..." />
-            <button>ارسال</button>
-          </span>
-          <span>
-            <p>شبکه های اجتماعی</p>
-            <Link to="">
-              {" "}
-              <BsTwitter color="#55acee" />
-            </Link>
-            <Link to="">
-              {" "}
-              <BsFacebook color="#3b5999" />
-            </Link>
-            <Link to="">
-              {" "}
-              <BsInstagram color="#e4405f" />
-            </Link>
-            <Link to="">
-              {" "}
-              <BsTelegram color="#0088cc" />
-            </Link>
-          </span>
-        </div>
-        <div>
-          <p>
-            ساخته شده با
-            <BsHeartFill color="red" />
-            توسط
-            <Link to="https://github.com/KhalilHashemi" target="_blank">
-              Khalil
-            </Link>
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
