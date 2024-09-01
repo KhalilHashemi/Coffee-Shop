fe3b242659a22152a1a95f1a11b7309baca7f825
Khalil <Khalilhashemi.h@gmail.com>
2024-09-01 12:51:22 +0330

    add header and footer layout components

---
+++ src/constants/navbar.js
@@ -0,0 +1,8 @@
+const navbarItems = [
+    { name: "خانه", id: 1, path: "/" },
+    { name: "کیک ها", id: 2, path: "/cakes" },
+    { name: "قهوه ها", id: 3, path: "/coffees" },
+    { name: "نوشیدنی ها", id: 4, path: "/drinks" },
+    { name: "درباره ما", id: 5, path: "/about-us" },
+]
+export { navbarItems }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,66 @@
+import { Link } from "react-router-dom";
+import { navbarItems } from "../constants/navbar";
+import {
+  BsTwitter,
+  BsTelegram,
+  BsInstagram,
+  BsFacebook,
+  BsHeartFill,
+} from "react-icons/bs";
+import styles from "../styles/Layout.module.css";
 function Layout({ children }) {
   return (
     <>
-      <header>header</header>
+      <header className={styles.header}>
+        <nav>
+          <ul>
+            {navbarItems.map((item) => (
+              <li key={item.id}>
+                <Link to={`${item.path}`}> {item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
       {children}
-      <footer>footer</footer>
+      <footer className={styles.footer}>
+        <div>
+          <span>
+            <p>عضویت در خبرنامه</p>
+            <input type="email" placeholder="لطفا ایمیل خود را وارد کنید..." />
+            <button>ارسال</button>
+          </span>
+          <span>
+            <p>شبکه های اجتماعی</p>
+            <Link to="">
+              {" "}
+              <BsTwitter color="#55acee" />
+            </Link>
+            <Link to="">
+              {" "}
+              <BsFacebook color="#3b5999" />
+            </Link>
+            <Link to="">
+              {" "}
+              <BsInstagram color="#e4405f" />
+            </Link>
+            <Link to="">
+              {" "}
+              <BsTelegram color="#0088cc" />
+            </Link>
+          </span>
+        </div>
+        <div>
+          <p>
+            ساخته شده با
+            <BsHeartFill color="red" />
+            توسط
+            <Link to="https://github.com/KhalilHashemi" target="_blank">
+              Khalil
+            </Link>
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
